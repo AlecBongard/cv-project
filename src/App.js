f00b338911857fbe3adfc10dec196a635ce3badb
Alec Bongard <alec.bongard@gmail.com>
2023-03-19 17:53:23 -0700

    Create education and work experience sections

--- src/App.js
+++ src/App.js
@@ -18,6 +18,60 @@ class App extends Component{
         labelText: "Last Name",
         type: "text",
       },
+      {
+        name: "phone",
+        labelText: "Phone Number",
+        type: "tel"
+      },
+      {
+        name: "email",
+        labelText: "email",
+        type: "email"
+      }
+      ]}></Section>
+      <Section sectionID="education" sectionName="Eductaion" fields={[
+        {
+          name: "school",
+          labelText: "School Name",
+          type: "text"
+        },
+        {
+          name: "major",
+          labelText: "Major",
+          type: "text"
+        },
+        {
+          name: "degree-type",
+          labelText: "Type of Degree",
+          type: "text",
+        },
+        {
+          name: "grad-date",
+          labelText: "Date of graduation",
+          type: "date",
+        }
+      ]}></Section>
+      <Section sectionID="experience" sectionName="Work Experience" fields={[
+        {
+          name: "company",
+          labelText: "Company Name",
+          type: "text",
+        },
+        {
+          name: "job-title",
+          labelText: "Job Title",
+          type: "text"
+        },
+        {
+          name: "start",
+          labelText: "Start Date",
+          type: "date",
+        },
+        {
+          name: "end",
+          labelText: "End Date",
+          type: "date",
+        }
       ]}></Section>
     </div>)
   }
