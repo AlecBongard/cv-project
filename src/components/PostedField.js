3a18cdd2c035de130f7d53ddb63c1165237e34f0
Alec Bongard <alec.bongard@gmail.com>
2023-04-03 14:27:00 -0700

    Change PostedField.js to a functional component

--- src/components/PostedField.js
+++ src/components/PostedField.js
@@ -1,27 +1,22 @@
 import react, { Component } from "react";
 import "../styles/posted.css";
 
-class PostedField extends Component {
-    constructor(props){
-        super(props);
+
+const PostedField = (props)=>{
+    let submittedText;
+
+    if(props.fieldText){
+        submittedText = props.fieldText;
+    }else{
+        submittedText = "N/A";
     }
 
-    render(){
-        let submittedText;
-
-        if(this.props.fieldText){
-            submittedText = this.props.fieldText;
-        }else{
-            submittedText = "N/A";
-        }
-
-        return (
-            <div className="field-posted">
-                <p className="label-posted">{this.props.labelText}</p>
-                <p className="text-posted">{submittedText}</p>
-            </div>
-        )
-    }
+    return (
+        <div className="field-posted">
+            <p className="label-posted">{props.labelText}</p>
+            <p className="text-posted">{submittedText}</p>
+        </div>
+    )
 }
 
 export default PostedField;
